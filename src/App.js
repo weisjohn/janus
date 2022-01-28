8f7f2e8962b3b92a73fdd447f27600a553be59db
John Weis <weis.john@gmail.com>
2022-01-28 17:19:04 -0500

    src: simple random counter, clicker, color

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Can we do this?
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import { Button, Card, Colors, Elevation } from "@blueprintjs/core";
+import React from "react";
+
+import Header from './Header'
+
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
+
+  handleButtonClick() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
+  // start counting
+  componentDidMount() {
+    let that = this;
+    setInterval(() => {
+      // randomly skip counting
+      if (!!(Math.random() > 0.5)) return;
+      that.setState({
+        count: this.state.count + 1,
+      });
+    }, 1e3);
+  }
+
+  render() {
+
+    let { count } = this.state;
+    let background = (count % 3 === 0) ? Colors.GREEN1:
+      (count % 2 === 0) ? Colors.BLUE1 : Colors.RED1;
+
+    return (
+      <div className="App">
+        <Header />
+
+        <div className="App-body">
+          <Card
+            style={({ background })}
+            className={`App-state`}
+            interactive={true}
+            elevation={Elevation.TWO}
+          >
+            <pre>{JSON.stringify(this.state)}</pre>
+          </Card>
+          <div />
+          <Button
+            className="bp3-intent-primary"
+            intent="success"
+            icon="plus"
+            onClick={this.handleButtonClick}
+            text={`Count`}
+          ></Button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
