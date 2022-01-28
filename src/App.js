2bbc5b7a8e1f56357a3dbfcefae2349250e8778f
John Weis <weis.john@gmail.com>
2022-01-28 15:34:03 -0500

    deps: use yarn not npm

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Can we do this?
         </a>
       </header>
     </div>
