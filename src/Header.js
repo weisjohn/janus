f86cc9a6617e330b1320fc7bb0c5156dfd1a900e
John Weis <weis.john@gmail.com>
2022-01-29 23:21:38 -0500

    src: header - handle roommates, add user identity

--- src/Header.js
+++ src/Header.js
@@ -8,8 +8,12 @@ import {
 } from "@blueprintjs/core";
 import { useSnapshot } from "valtio";
 
+import Identity from './Identity';
+
 function Header({ local }) {
   const snap = useSnapshot(local);
+
+  const { user, roommates } = snap;
 
   return (
     <Navbar>
@@ -20,8 +24,7 @@ function Header({ local }) {
         </Navbar.Heading>
         <Navbar.Divider />
         {"looking both directions"}
-      </Navbar.Group>
-      <Navbar.Group align={Alignment.RIGHT}>
+        <Navbar.Divider />
         <ButtonGroup>
           <Button
             icon={snap.generate ? "pause" : "play"}
@@ -31,6 +34,11 @@ function Header({ local }) {
             text={snap.generate ? "Pause" : "Generate"}
           />
         </ButtonGroup>
+      </Navbar.Group>
+      <Navbar.Group align={Alignment.RIGHT}>
+        { roommates.map(u => <Identity key={u.uuid} user={u} />) }
+        <Navbar.Divider />
+        <Identity user={user} type="full" />
       </Navbar.Group>
     </Navbar>
   );
