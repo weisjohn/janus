0cf4452c24257ee9af41c7b9d0983aa6708255f5
John Weis <weis.john@gmail.com>
2022-02-01 23:04:51 -0500

    src: header - add a comms spinner to indicate synchronization

--- src/Header.js
+++ src/Header.js
@@ -6,6 +6,8 @@ import {
   Button,
   ButtonGroup,
   Position,
+  Spinner,
+  SpinnerSize,
 } from "@blueprintjs/core";
 import { Tooltip2 } from "@blueprintjs/popover2";
 import { useSnapshot } from "valtio";
@@ -15,25 +17,30 @@ import Identity from './Identity';
 function Header({ local, provider }) {
   const snap = useSnapshot(local);
 
-  const { user, roommates } = snap;
+  const { user, roommates, connected, synced } = snap;
 
   return (
     <Navbar>
       <Navbar.Group align={Alignment.LEFT}>
         <Navbar.Heading>
-          <Tooltip2 content={"looking both directions"} position={Position.DOWN}>
-            <Tag icon="resolve" large minimal>JANUS</Tag>
+          <Tooltip2
+            content={"looking both directions"}
+            position={Position.DOWN}
+          >
+            <Tag icon="resolve" large minimal>
+              JANUS
+            </Tag>
           </Tooltip2>
         </Navbar.Heading>
         <Navbar.Divider />
         <ButtonGroup>
           <Button
-            icon={snap.connected ? "cell-tower" : "disable"}
-            intent={snap.connected ? "success" : "danger"}
-            loading={snap.connected == null}
+            icon={connected ? "cell-tower" : "disable"}
+            intent={connected ? "success" : "danger"}
+            loading={connected == null}
             outlined
             onClick={() => {
-              if (snap.connected) {
+              if (connected) {
                 provider.disconnect();
                 local.connected = null;
               } else {
@@ -41,12 +48,23 @@ function Header({ local, provider }) {
                 local.connected = null;
               }
             }}
-            text={snap.connected ? "Connected" : "Disconnected"}
+            text={connected ? "Connected" : "Disconnected"}
           />
         </ButtonGroup>
+        {connected && (
+          <div style={{ marginLeft: "0.6rem" }}>
+            <Spinner
+              intent="success"
+              size={SpinnerSize.SMALL}
+              value={ synced ? 1 : null }
+            />
+          </div>
+        )}
       </Navbar.Group>
       <Navbar.Group align={Alignment.RIGHT}>
-        { roommates.map(u => <Identity key={u.uuid} user={u} />) }
+        {roommates.map((u) => (
+          <Identity key={u.uuid} user={u} />
+        ))}
         <Navbar.Divider />
         <Identity user={user} type="full" />
       </Navbar.Group>
